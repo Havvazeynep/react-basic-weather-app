129780f78913892f9a402065232165ffe50f5206
Havva Zeynep Akdemir <83979496+Havvazeynep@users.noreply.github.com>
2022-10-02 22:07:17 +0300

    basit hava durumu uygulaması

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import City from './City';
 
 function App() {
+  //https://openweathermap.org/
+  const key = "b4557f4bbd5861ec87d9b055858915bb";
+  const[search, setSearch] = useState("");
+  const[city, setCity] = useState();
+  
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`);
+        console.log(response);
+        setCity(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getApi();
+  },[search]);
+  console.log(search);
   return (
-    <div className="App">
-      hello
+  <div className='App'>
+    <div>
+      <input onKeyDown={(e) => setSearch(e.target.value)} type='text' className=" my-5 px-3 w-[250px] py-3 placeholder-blueGray-300 text-blueGray-600   bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring "/>
+      {city && <City city={city} />}
     </div>
+  </div>
   );
 }
 
